0955cea730719b251a2e852a240326641a6e150e
Patryk Kups <pkups@anaconda.com>
2025-08-13 18:15:38 +0200

    test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
-
+@REM This script is a rewritten version of deps.sh.
 @REM https://github.com/lexiforest/curl-impersonate/blob/v1.2.0/win/deps.sh
+@echo on
+
 mkdir deps
-@echo off
-setlocal enabledelayedexpansion
 
 @REM https://github.com/lexiforest/curl-impersonate/blob/v1.2.0/.github/workflows/build-win.yaml#L18
 set ZLIB_COMMIT=09155eaa2f9270dc4ed1fa13e2b4b2613e6e4851
@@ -14,41 +14,78 @@ set NGHTTP2_COMMIT=8f44147c385fb1ed93a6f39911eeb30279bfd2dd
 set NGHTTP3_TAG=v1.9.0
 set NGTCP2_TAG=v1.11.0
 
-call :get_dep "https://github.com/madler/zlib.git" "zlib" "%ZLIB_COMMIT%"
-call :get_dep "https://github.com/madler/zlib.git" "zlib" "%ZLIB_COMMIT%"
-call :get_dep "https://github.com/facebook/zstd.git" "zstd" "%ZSTD_COMMIT%"
-call :get_dep "https://github.com/google/brotli.git" "brotli" "%BROTLI_COMMIT%"
-call :get_dep "https://boringssl.googlesource.com/boringssl.git" "boringssl" "%BORINGSSL_COMMIT%"
-call :get_dep "https://github.com/curl/curl.git" "curl" "%CURL_TAG%"
-call :get_dep "https://github.com/nghttp2/nghttp2.git" "nghttp2" "%NGHTTP2_COMMIT%"
-call :get_dep "https://github.com/ngtcp2/nghttp3.git" "nghttp3" "%NGHTTP3_TAG%"
-call :get_dep "https://github.com/ngtcp2/ngtcp2.git" "ngtcp2" "%NGTCP2_TAG%"
+@REM --- Download zlib ---
+git clone "https://github.com/madler/zlib.git" "deps\zlib"
+if errorlevel 1 exit 1
+git -C "deps\zlib" checkout "%ZLIB_COMMIT%"
+if errorlevel 1 exit 1
+
+@REM --- Download zstd ---
+git clone "https://github.com/facebook/zstd.git" "deps\zstd"
+if errorlevel 1 exit 1
+git -C "deps\zstd" checkout "%ZSTD_COMMIT%"
+if errorlevel 1 exit 1
+
+@REM --- Download brotli ---
+git clone "https://github.com/google/brotli.git" "deps\brotli"
+if errorlevel 1 exit 1
+git -C "deps\brotli" checkout "%BROTLI_COMMIT%"
+if errorlevel 1 exit 1
+
+@REM --- Download boringssl ---
+git clone "https://boringssl.googlesource.com/boringssl.git" "deps\boringssl"
+if errorlevel 1 exit 1
+git -C "deps\boringssl" checkout "%BORINGSSL_COMMIT%"
+if errorlevel 1 exit 1
+
+@REM --- Download curl ---
+git clone "https://github.com/curl/curl.git" "deps\curl"
+if errorlevel 1 exit 1
+git -C "deps\curl" checkout "%CURL_TAG%"
+if errorlevel 1 exit 1
+
+@REM --- Download nghttp2 ---
+git clone "https://github.com/nghttp2/nghttp2.git" "deps\nghttp2"
+if errorlevel 1 exit 1
+git -C "deps\nghttp2" checkout "%NGHTTP2_COMMIT%"
+if errorlevel 1 exit 1
+
+@REM --- Download nghttp3 ---
+git clone "https://github.com/ngtcp2/nghttp3.git" "deps\nghttp3"
+if errorlevel 1 exit 1
+git -C "deps\nghttp3" checkout "%NGHTTP3_TAG%"
+if errorlevel 1 exit 1
+
+@REM --- Download ngtcp2 ---
+git clone "https://github.com/ngtcp2/ngtcp2.git" "deps\ngtcp2"
+if errorlevel 1 exit 1
+git -C "deps\ngtcp2" checkout "%NGTCP2_TAG%"
+if errorlevel 1 exit 1
 
 @REM --- Update submodules ---
 echo Updating submodules...
 git -C "deps\nghttp2" submodule update --init
+if errorlevel 1 exit 1
 git -C "deps\nghttp3" submodule update --init
+if errorlevel 1 exit 1
 git -C "deps\ngtcp2" submodule update --init
+if errorlevel 1 exit 1
 
 @REM --- Apply patches ---
+@REM https://github.com/lexiforest/curl-impersonate/tree/v1.2.0/patches
 echo Applying patches...
 patch -p1 -d "deps\boringssl" < "patches\boringssl.patch"
+if errorlevel 1 exit 1
 patch -p1 -d "deps\curl" < "patches\curl.patch"
+if errorlevel 1 exit 1
 patch -p1 < "win\deps.patch"
+if errorlevel 1 exit 1
 
-%SRC_DIR%\win\build.bat
+call %SRC_DIR%\win\build.bat
 
-@REM --- function get_dep ---
-@REM %1 = repo URL
-@REM %2 = destination
-@REM %3 = commit
-:get_dep
-git clone "%~1" "deps\%~2"
-if not "%~3"=="" (
-    git -C "deps\%~2" checkout "%~3"
-)
-goto :eof
-
-
-endlocal
-
+robocopy "%SRC_DIR%\packages\bin" "%LIBRARY_BIN%" /E
+if %ERRORLEVEL% GEQ 8 exit 1
+robocopy "%SRC_DIR%\packages\include" "%LIBRARY_INC%" /E
+if %ERRORLEVEL% GEQ 8 exit 1
+robocopy "%SRC_DIR%\packages\lib" "%LIBRARY_LIB%" /E
+if %ERRORLEVEL% GEQ 8 exit 1
