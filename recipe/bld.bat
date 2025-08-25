2c11e5d0cf10a792cbfc745aafe4b8511b1b53d9
Patryk Kups <pkups@anaconda.com>
2025-08-25 16:23:52 +0200

    test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,26 +5,12 @@
 mkdir deps
 
 @REM https://github.com/lexiforest/curl-impersonate/blob/v1.2.0/.github/workflows/build-win.yaml#L18
-set ZLIB_COMMIT=09155eaa2f9270dc4ed1fa13e2b4b2613e6e4851
-set ZSTD_COMMIT=794ea1b0afca0f020f4e57b6732332231fb23c70
 set BROTLI_COMMIT=ed738e842d2fbdf2d6459e39267a633c4a9b2f5d
 set BORINGSSL_COMMIT=673e61fc215b178a90c0e67858bbf162c8158993
 set CURL_TAG=curl-8_15_0
 set NGHTTP2_COMMIT=8f44147c385fb1ed93a6f39911eeb30279bfd2dd
 set NGHTTP3_TAG=v1.9.0
 set NGTCP2_TAG=v1.11.0
-
-@REM --- Download zlib ---
-git clone "https://github.com/madler/zlib.git" "deps\zlib"
-if errorlevel 1 exit 1
-git -C "deps\zlib" checkout "%ZLIB_COMMIT%"
-if errorlevel 1 exit 1
-
-@REM --- Download zstd ---
-git clone "https://github.com/facebook/zstd.git" "deps\zstd"
-if errorlevel 1 exit 1
-git -C "deps\zstd" checkout "%ZSTD_COMMIT%"
-if errorlevel 1 exit 1
 
 @REM --- Download brotli ---
 git clone "https://github.com/google/brotli.git" "deps\brotli"
